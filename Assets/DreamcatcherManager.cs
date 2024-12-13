70c338bb37b4c58567d75914c31aae1006798ccc
alya1007 <alexandra.konjevic@isa.utm.md>
2024-12-14 00:08:25 +0200

    add dreamcatcher counter

--- Assets/DreamcatcherManager.cs
+++ Assets/DreamcatcherManager.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DreamcatcherManager : MonoBehaviour
 {
     public int dreamcatcherCount = 0;
+    public Text dreamcatcherCountText;
     void Start()
     {
         
@@ -11,6 +13,6 @@ public class DreamcatcherManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        dreamcatcherCountText.text = "Count: " + dreamcatcherCount.ToString();
     }
 }
